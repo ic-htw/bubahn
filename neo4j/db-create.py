54a0ac6ab277ba959d9ac034e30bf845c1e7fa41
Ingo Classen <ingo.classen@htw-berlin.de>
2023-03-14 15:57:40 +0100

    wip

--- neo4j/db-create.py
+++ neo4j/db-create.py
@@ -2,8 +2,8 @@ from neo4j import GraphDatabase
 import cred_neo4j as c
 
 def exe1(driver, *qs):
-    # pass
-    exe(driver, *qs)
+    pass
+    # exe(driver, *qs)
     
 def exe2(driver, *qs):
     # pass
@@ -43,8 +43,8 @@ with GraphDatabase.driver(c.neo4j_host, auth=(c.neo4j_userid, c.neo4j_password))
     CREATE CONSTRAINT c_haltestelle FOR (x:Haltestelle) REQUIRE x.id IS UNIQUE;
     """
 
-    # exe1(driver, q1, q2)  
-    # exe2(driver, q3, q4)  
+    exe1(driver, q1, q2)  
+    exe2(driver, q3, q4)  
 
 
     # -------------------------------------------------
@@ -74,12 +74,12 @@ with GraphDatabase.driver(c.neo4j_host, auth=(c.neo4j_userid, c.neo4j_password))
     CREATE CONSTRAINT c_linie FOR (x:Linie) REQUIRE x.id IS UNIQUE;
     """
 
-    # exe1(driver, q1, q2)  
-    # exe2(driver, q3, q4)  
+    exe1(driver, q1, q2)  
+    exe2(driver, q3, q4)  
 
 
     # -------------------------------------------------
-    # 02 Segment
+    # 03 Segment
     # -------------------------------------------------
     print("03 Segment")
     q2 = f"""
@@ -102,33 +102,174 @@ with GraphDatabase.driver(c.neo4j_host, auth=(c.neo4j_userid, c.neo4j_password))
     exe2(driver, q3)  
 
 
-
-
-
-
-
-
-
     # -------------------------------------------------
-    # 03 xxx
+    # 04 Unterlinie
     # -------------------------------------------------
-    print("13 IS_LOCATED_IN")
+    print("04 Unterlinie")
     q1 = f"""
-    MATCH (x:Company)-[z:IS_LOCATED_IN]->(y:Country) DETACH DELETE z;
+    DROP CONSTRAINT c_unterlinie;
+    """
+    
+    q2 = f"""
+    MATCH (x:Unterlinie) DETACH DELETE x;
     """
 
-    q2 = f"""
-    LOAD CSV WITH HEADERS FROM 'file:///sozmed/Company.csv' AS r FIELDTERMINATOR '|'
+    q3 = f"""
+    LOAD CSV WITH HEADERS FROM 'file:///bubahn/unterlinie.csv' AS r FIELDTERMINATOR ','
     CALL {{
       WITH r
-      MATCH (x:Company {{id: toInteger(r.id)}})
-      MATCH (y:Country {{id: toInteger(r.LocationPlaceId)}})
-      CREATE (x)-[:IS_LOCATED_IN]->(y)
+      CREATE (x:Unterlinie {{
+        ulid: toInteger(r.ULID), 
+        lid: toInteger(r.LID)
+        }})
     }} IN TRANSACTIONS OF 100000 ROWS;
     """
 
-    # exe1(driver, q1)  
-    # exe2(driver, q2)  
+    q4 = f"""
+    CREATE CONSTRAINT c_unterlinie FOR (x:Unterlinie) REQUIRE x.id IS UNIQUE;
+    """
+
+    exe1(driver, q1, q2)  
+    exe2(driver, q3, q4)  
+
+    # -------------------------------------------------
+    # 05 Abschnitt
+    # -------------------------------------------------
+    print("05 Abschnitt")
+    q2 = f"""
+    MATCH (x:Abschnitt) DETACH DELETE x;
+    """
+
+    q3 = f"""
+    LOAD CSV WITH HEADERS FROM 'file:///bubahn/abschnitt.csv' AS r FIELDTERMINATOR ','
+    CALL {{
+      WITH r
+      CREATE (x:Abschnitt {{
+        ulid: toInteger(r.ULID), 
+        nr: toInteger(r.NR), 
+        hid_a: toInteger(r.HID_A), 
+        hid_b: toInteger(r.HID_B), 
+        haelt: r.HAELT
+        }})
+    }} IN TRANSACTIONS OF 100000 ROWS;
+    """
+
+    exe1(driver, q2)  
+    exe2(driver, q3)  
+
+
+    # -------------------------------------------------
+    # 06 ProjSegA
+    # -------------------------------------------------
+    print("06 ProjSegA")
+    q1 = f"""
+    MATCH (x:Segment)-[z:ProjSegA]->(y:Haltestelle) DETACH DELETE z;
+    """
+
+    q2 = f"""
+    MATCH (x:Segment)
+    WITH x, x.hid_a as hid_a, x.hid_b as hid_b
+    MATCH (y:Haltestelle {{hid: hid_a}})
+    CREATE (x)-[:ProjSegA]->(y)
+    """
+
+    exe1(driver, q1)  
+    exe2(driver, q2)  
+
+
+    # -------------------------------------------------
+    # 07 ProjSegB
+    # -------------------------------------------------
+    print("07 ProjSegB")
+    q1 = f"""
+    MATCH (x:Segment)-[z:ProjSegB]->(y:Haltestelle) DETACH DELETE z;
+    """
+
+    q2 = f"""
+    MATCH (x:Segment)
+    WITH x, x.hid_a as hid_a, x.hid_b as hid_b
+    MATCH (y:Haltestelle {{hid: hid_b}})
+    CREATE (x)-[:ProjSegB]->(y)
+    """
+
+    exe1(driver, q1)  
+    exe2(driver, q2)  
+
+
+    # -------------------------------------------------
+    # 08 InL
+    # -------------------------------------------------
+    print("08 InL")
+    q1 = f"""
+    MATCH (x:Unterlinie)-[z:InL]->(y:Linie) DETACH DELETE z;
+    """
+
+    q2 = f"""
+    MATCH (x:Unterlinie)
+    WITH x, x.lid as lid
+    MATCH (y:Linie {{lid: lid}})
+    CREATE (x)-[:InL]->(y)
+    """
+
+    exe1(driver, q1)  
+    exe2(driver, q2)  
+
+
+    # -------------------------------------------------
+    # 09 InUL
+    # -------------------------------------------------
+    print("09 InUL")
+    q1 = f"""
+    MATCH (x:Abschnitt)-[z:InUL]->(y:Unterlinie) DETACH DELETE z;
+    """
+
+    q2 = f"""
+    MATCH (x:Abschnitt)
+    WITH x, x.ulid as ulid
+    MATCH (y:Unterlinie {{ulid: ulid}})
+    CREATE (x)-[:InUL]->(y)
+    """
+
+    exe1(driver, q1)  
+    exe2(driver, q2)  
+
+
+    # -------------------------------------------------
+    # 10 ProjAbA
+    # -------------------------------------------------
+    print("10 ProjAbA")
+    q1 = f"""
+    MATCH (x:Abschnitt)-[z:ProjAbA]->(y:Haltestelle) DETACH DELETE z;
+    """
+
+    q2 = f"""
+    MATCH (x:Abschnitt)
+    WITH x, x.hid_a as hid_a, x.hid_b as hid_b
+    MATCH (y:Haltestelle {{hid: hid_a}})
+    CREATE (x)-[:ProjAbA]->(y)
+    """
+
+    exe1(driver, q1)  
+    exe2(driver, q2)  
+
+
+    # -------------------------------------------------
+    # 11 ProjAbB
+    # -------------------------------------------------
+    print("11 ProjAbA")
+    q1 = f"""
+    MATCH (x:Abschnitt)-[z:ProjAbB]->(y:Haltestelle) DETACH DELETE z;
+    """
+
+    q2 = f"""
+    MATCH (x:Abschnitt)
+    WITH x, x.hid_a as hid_a, x.hid_b as hid_b
+    MATCH (y:Haltestelle {{hid: hid_b}})
+    CREATE (x)-[:ProjAbB]->(y)
+    """
+
+    exe1(driver, q1)  
+    exe2(driver, q2)  
 
 
 # -------------------------------------------------
